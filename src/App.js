89197ffeffcd606bfd64f7284ec6d49e5e191732
Waldemar Mikluszka <wmikluszka@outlook.com>
2020-06-20 19:46:45 +0200

    Buttons added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      clicks: 0
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <output>Klinięcia: {this.state.clicks}</output>
+        <button onClick={this.increment.bind(this)}>Kliknij</button>
+      </div>
+    );
+  }
+
+  increment() {
+    console.log("was", this.state.clicks);
+    this.setState({
+      clicks: this.state.clicks + 1
+    }, () => {
+      console.log("is", this.state.clicks)
+    });
+  }
+}
+
+/*
+class App extends React.Component {
+  render() {
+    return <button onClick={this.onClickHandler}>Kliknij  Myszką!</button>;
+  }
+  onClickHandler() {
+    alert("Kliknięto!");
+  }
+}
+*/
+
+/*
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => alert('Kliknięto!')}>Kliknij!</button>
       </header>
     </div>
   );
 }
+*/
 
 export default App;
